617a16297b316df948b3b19dc0f91fff22f42b51
神楽坂雅詩 <cyscxchope@gmail.com>
2022-08-27 18:25:40 +0800

    基本框架

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import YaTopAppBar from './yaui/ya-top-app-bar/ya-top-app-bar';
+import YaIconButton from './yaui/ya-icon-button/ya-icon-button';
+
+export default class Main {
+  constructor() {
+  }
+}
