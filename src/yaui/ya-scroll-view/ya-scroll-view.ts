030600a1327732779f5dca77913dba485af9c5b6
神楽坂雅詩 <cyscxchope@gmail.com>
2022-09-06 18:27:37 +0800

    ya-scroll-view的滚动可使ya-top-app-bar自动收起

--- src/yaui/ya-scroll-view/ya-scroll-view.ts
+++ src/yaui/ya-scroll-view/ya-scroll-view.ts
@@ -3,6 +3,10 @@
  */
 export default class YaScrollView extends HTMLElement {
     static control = "ya-scroll-view";
+    scrollOldTop: number = 0;
+    scrollOldMode: boolean = false; // F↑ T↓
+    scrollModeI: number = 0;
+    appBars: HTMLElement[] = [];
 
     /**
      * 加载所需的样式等其他文件
@@ -18,6 +22,69 @@ export default class YaScrollView extends HTMLElement {
     constructor() {
         super();
         this.className = YaScrollView.control + " " + this.className;
-        // this.addEventListener("scroll", () => {});
+        if (this.className.indexOf("ya-auto-hide-app-bar") >= 0) {
+            const appBarEs: HTMLCollectionOf<Element> =
+                document.body.getElementsByTagName("ya-top-app-bar");
+            for (const key in appBarEs) {
+                if (Object.prototype.hasOwnProperty.call(appBarEs, key)) {
+                    const appBar: HTMLElement = appBarEs[key] as HTMLElement;
+                    appBar.style.transition = "all 0.5s";
+                    this.appBars.push(appBar);
+                }
+            }
+            this.addEventListener("scroll", () => {
+                const top: number = this.scrollTop;
+                if (this.scrollOldTop > top) {
+                    console.log("↑", top);
+                    if (!this.scrollOldMode) {
+                        this.scrollModeI++;
+                        if (this.scrollModeI > 10) {
+                            this.appBarShow(true);
+                        }
+                    } else {
+                        this.scrollModeI = 0;
+                    }
+                    this.scrollOldMode = false;
+                } else if (this.scrollOldTop < top) {
+                    console.log("↓", top);
+                    if (this.scrollOldMode) {
+                        this.scrollModeI++;
+                        if (this.scrollModeI > 10) {
+                            this.appBarShow(false);
+                        }
+                    } else {
+                        this.scrollModeI = 0;
+                    }
+                    this.scrollOldMode = true;
+                }
+                if (top >= 70) {
+                }
+                this.scrollOldTop = top;
+            });
+        }
+    }
+
+    appBarShow(isShow: boolean) {
+        if (isShow) {
+            for (const appBar of this.appBars) {
+                appBar.className = appBar.className.replace(
+                    "top-app-bar-hide",
+                    "top-app-bar-show"
+                );
+                if (appBar.className.indexOf("top-app-bar-show") < 0) {
+                    appBar.className += "top-app-bar-show";
+                }
+            }
+        } else {
+            for (const appBar of this.appBars) {
+                appBar.className = appBar.className.replace(
+                    "top-app-bar-show",
+                    "top-app-bar-hide"
+                );
+                if (appBar.className.indexOf("top-app-bar-hide") < 0) {
+                    appBar.className += "top-app-bar-hide";
+                }
+            }
+        }
     }
 }
