bcdcdd12bd36830918aab4af66c0174523239fd1
神楽坂雅詩 <cyscxchope@gmail.com>
2022-09-25 18:35:09 +0800

    开关控件

---
+++ ya-switch/ya-switch.ts
@@ -0,0 +1,54 @@
+/**
+ * 滑塊開關
+ */
+export default class YaSwitch extends HTMLElement {
+    static control = "ya-switch";
+
+    /**
+     * 加载所需的样式等其他文件
+     * 該函式由 YAUI 類呼叫，無需手工執行
+     */
+    static loadFile() {
+        require(`./${YaSwitch.control}.css`);
+    }
+
+    /**
+     * 對該控制元件的 UI 和行為進行準備工作
+     */
+    constructor() {
+        super();
+        this.className = YaSwitch.control + " " + this.className;
+        const label: HTMLLabelElement = document.createElement("label");
+        label.className = YaSwitch.control + "-label";
+        // <input type="checkbox" checked>
+        const input: HTMLInputElement = document.createElement("input");
+        input.className = YaSwitch.control + "-input";
+        input.type = "checkbox";
+        input.checked = this.hasAttribute("value")
+            ? this.getAttribute("value") == "0"
+                ? false
+                : true
+            : false;
+        this.setAttribute("value", input.checked ? "1" : "0");
+        input.readOnly = true;
+        label.appendChild(input);
+        const span: HTMLSpanElement = document.createElement("span");
+        span.className = YaSwitch.control + "-span";
+        const i: HTMLElement = document.createElement("i");
+        i.className = YaSwitch.control + "-i";
+        span.appendChild(i);
+        label.appendChild(span);
+        const width: number = this.getAttribute("size")
+            ? parseInt(this.getAttribute("size"))
+            : 40;
+        const height: number = width / 2;
+        label.style.width = width + "px";
+        label.style.height = height + "px";
+        span.style.borderRadius = height + "px";
+        this.insertBefore(label, this.firstChild);
+        this.addEventListener("click", (e) => {
+            input.checked = !input.checked;
+            this.setAttribute("value", input.checked ? "1" : "0");
+        });
+    }
+}
