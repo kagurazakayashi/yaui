b849171c28c2d3b86f0236b2a18c8df048ecb6cd
神楽坂雅詩 <cyscxchope@gmail.com>
2022-10-01 18:25:41 +0800

    Snackbar支持从屏幕四角弹出

--- ya-snackbar/ya-snackbar.ts
+++ ya-snackbar/ya-snackbar.ts
@@ -11,25 +11,48 @@ export default class YaSnackbar {
 
     /**
      * 顯示 Snackbar
+     * @param {string} text 要顯示的文字
+     * @param {string} title 標題
+     * @param {string} icon `圖標名称` 或 `图标名称#图标颜色`
+     * 如果未指定 `#图标颜色` ，将根据图标名称自动选择颜色：
+     * check_circle:forestgreen, cancel:orangered, warning:goldenrod
+     * @param {string} position 在螢幕上的位置
+     * lt: 左上, rt: 右上, lb: 左下, rb: 右下
+     * @param {number} duration 懸停時間
+     * @param {number} width 提示框寬度（會改變一側所有提示框的寬度）
+     * @param {number} speed 進入和退出動畫的速度
      */
     static show(
         text: string,
         title: string = "",
         icon: string = "info",
-        duration: number = 3000
+        position: string = "rt",
+        duration: number = 3000,
+        width: number = 300,
+        speed: number = 300
     ) {
+        position = position.toLowerCase();
+        const isLeft: boolean = position.indexOf("l") >= 0;
+        const isTop: boolean = position.indexOf("t") >= 0;
+        const className: string = `${YaSnackbar.control}-box-${
+            isLeft ? "l" : "r"
+        } ${YaSnackbar.control}-box-${isTop ? "t" : "b"}`;
         const snackboxs: HTMLCollectionOf<Element> =
-            document.getElementsByClassName(YaSnackbar.control + "-box");
+            document.getElementsByClassName(className);
         let snackbox: HTMLDivElement;
         if (snackboxs.length == 0) {
             snackbox = document.createElement("div");
-            snackbox.className = YaSnackbar.control + "-box";
+            snackbox.className = `${className} ${YaSnackbar.control}-box`;
             document.body.appendChild(snackbox);
         } else {
             snackbox = snackboxs[0] as HTMLDivElement;
         }
+        snackbox.style.width = width + "px";
         const snackbar: HTMLSpanElement = document.createElement("span");
-        snackbar.className = YaSnackbar.control + " ya-share-box";
+        snackbar.className = `${YaSnackbar.control} ${YaSnackbar.control}-${
+            isLeft ? "l" : "r"
+        } ya-share-box`;
+        snackbar.style.transitionDuration = speed / 1000 + "s";
         const bg: HTMLSpanElement = document.createElement("span");
         bg.className = "ya-share-box-bg";
         snackbar.appendChild(bg);
@@ -71,34 +94,39 @@ export default class YaSnackbar {
         }
         snackbar.appendChild(textBox);
         snackbox.appendChild(snackbar);
-        const moveX: number = snackbar.clientWidth + 20;
+        const owidth = snackbar.offsetWidth;
+        const moveX: number[] = isLeft
+            ? [owidth + 40, 0 - owidth - 20]
+            : [0 - owidth - 20, owidth + 20];
         setTimeout(() => {
             if (snackbar)
-                snackbar.style.transform = `translate(-${moveX}px, 0)`;
+                snackbar.style.transform = `translate(${moveX[0]}px, 0)`;
         }, 100);
-        const closeTransform: string = `translate(${moveX}px, 0)`;
+        const closeTransform: string = `translate(${moveX[1]}px, 0)`;
         setTimeout(() => {
             if (snackbar) snackbar.style.transform = closeTransform;
         }, 100 + duration);
         setTimeout(() => {
             if (snackbar) snackbar.remove();
-            YaSnackbar.autoRemoveBox();
-        }, 100 + duration + 300);
+            YaSnackbar.autoRemoveBox(isLeft);
+        }, 100 + duration + speed);
         snackbar.addEventListener("click", () => {
             if (snackbar) snackbar.style.transform = closeTransform;
             setTimeout(() => {
                 if (snackbar) snackbar.remove();
-                YaSnackbar.autoRemoveBox();
-            }, 300);
+                YaSnackbar.autoRemoveBox(isLeft);
+            }, speed);
         });
     }
 
-    static autoRemoveBox() {
+    static autoRemoveBox(isLeft: boolean) {
         const snackbars: HTMLCollectionOf<Element> =
             document.getElementsByClassName(YaSnackbar.control);
         if (snackbars.length == 0) {
             const snackboxs: HTMLCollectionOf<Element> =
-                document.getElementsByClassName(YaSnackbar.control + "-box");
+                document.getElementsByClassName(
+                    `${YaSnackbar.control}-box-${isLeft ? "l" : "r"}`
+                );
             for (const key in snackboxs) {
                 if (Object.prototype.hasOwnProperty.call(snackboxs, key)) {
                     const snackbox: Element = snackboxs[key];
